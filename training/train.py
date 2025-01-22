5b638372e0d0c77bb7fdd9b7a03d86def0395f79
SarahMuth <sarahmuth9@gmail.com>
2025-01-22 18:21:35 +0100

    implementing calling supervised training

--- training/train.py
+++ training/train.py
@@ -3,6 +3,7 @@ from glob import glob
 import argparse
 
 from utils.dataset_splits import get_paths
+from utils.training import supervised_training
 
 TRAIN_ROOT = ""
 LABEL_ROOT = ""
@@ -21,8 +22,37 @@ def train(key, ignore_label = None, training_2D = False, testset = True, extensi
     else:
         test_paths, test_label_paths = None, None
 
-    #TODO call supervised training from utils.training
+    print("Start training with:")
+    print(len(train_paths), "tomograms for training")
+    print(len(val_paths), "tomograms for validation")
 
+    patch_shape = [, , ] #TODO
+    model_name="" #TODO
+
+    batch_size = 4 #TODO
+    check = False
+
+    #TODO do we want n_samples_train and n_samples_val in the supervised training?
+    supervised_training(
+        name=model_name,
+        train_paths=train_paths,
+        train_label_paths = train_label_paths,
+        val_paths=val_paths,
+        val_label_paths = val_label_paths,
+        test_paths=test_paths,
+        test_label_paths=test_label_paths,
+        patch_shape=patch_shape, batch_size=batch_size,
+        check=check,
+        save_root="/mnt/lustre-emmy-hdd/usr/u12095/synapse_net/models_v2",
+        lr=,#TODO
+        n_iterations=,#TODO
+        out_channels=,#TODO 
+        augmentations=,#TODO 
+        eps=, #TODO 
+        sigma=, #TODO 
+        lower_bound=, #TODO
+        upper_bound=,#TODO 
+    )
 
 
 def main():
