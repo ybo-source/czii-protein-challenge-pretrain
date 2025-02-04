fb0aec0fee0c391c6ef9f0774f052e2d115a9263
SarahMuth <sarahmuth9@gmail.com>
2025-02-04 15:50:42 +0100

    1st implementation to compute _len

---
+++ utils/image.py
@@ -0,0 +1,32 @@
+import os
+import zarr
+import numpy as np
+from typing import Union, Sequence
+
+def load_image(image_path):
+    """@private
+    """
+    #TODO make flexible for other data types
+    return zarr.open(os.path.join(image_path, "VoxelSpacing10.000", "denoised.zarr", "0"), mode='r')
+
+    
+def load_data(
+    path: Union[str, Sequence[str]],
+) -> ArrayLike:
+    """Load data from a file or multiple files.
+
+    Args:
+        path: The file path or paths to the data.
+
+    Returns:
+        The loaded data.
+    """
+    #TODO can expand to also read in a key (check torch-em/torch_em/util/image.py)
+
+    have_single_file = isinstance(path, str)
+    have_single_key = isinstance(key, str)
+
+    if have_single_file:
+        return load_image(path)
+    else:
+        return np.stack([load_image(p) for p in path])
--- utils/training/heatmap_dataset.py
+++ utils/training/heatmap_dataset.py
@@ -7,30 +7,39 @@ import zarr
 import os
 
 from data_processing.create_heatmap import create_heatmap
+from .. import load_data
 
 
 class HeatmapDataset(torch.utils.data.Dataset):
     max_sampling_attempts = 500
 
+    @staticmethod
+    def compute_len(shape, patch_shape):
+        if patch_shape is None:
+            return 1
+        else:
+            n_samples = int(np.prod([float(sh / csh) for sh, csh in zip(shape, patch_shape)]))
+            return n_samples
+
     def __init__(
         self,
-        raw_image_paths,
-        label_paths,
-        patch_shape,
-        raw_transform=None,
-        label_transform=None,
-        transform=None,
-        dtype=torch.float32,
-        label_dtype=torch.float32,
-        n_samples=None,
-        sampler=None,
-        eps=10**-8,
-        sigma=None,
-        lower_bound=None,
-        upper_bound=None
+        raw_image_paths: Union[List[Any], str, os.PathLike],
+        label_paths: Union[List[Any], str, os.PathLike],
+        patch_shape: Tuple[int, ...],
+        raw_transform: Optional[Callable] = None,
+        label_transform: Optional[Callable] = None,
+        transfor: Optional[Callable] = None,
+        dtype: torch.dtype = torch.float32,
+        label_dtype: torch.dtype = torch.float32,
+        n_samples: Optional[int] = None,
+        sampler: Optional[Callable] = None,
+        eps: Optional[float] = 10**-8,
+        sigma: Optional[float] = None,
+        lower_bound: Optional[float] = None,
+        upper_bound: Optional[float] = None
     ):
-        self.raw_images = raw_image_paths
-        self.label_images = label_paths
+        self.raw_images = load_data(raw_image_paths) #TODO check if this is right
+        self.label_paths = label_paths
         self._ndim = 3
 
         assert len(patch_shape) == self._ndim
@@ -50,14 +59,13 @@ class HeatmapDataset(torch.utils.data.Dataset):
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
 
-        # TODO: n_samples should be derived from how often the bounding box fits
-        # rather than the number of volumes (as in SegmentationDataset)
-        if n_samples is None:
-            self._len = len(self.raw_images)
-            self.sample_random_index = False
-        else:
-            self._len = n_samples
-            self.sample_random_index = True
+        # TODO: check if this is right?
+        self.raw_images = np.array(self.raw_images) if not isinstance(self.raw_images, np.ndarray) else self.raw_images
+        self.shape = self.raw_images[1:].shape
+        self._len = self.compute_len(self.shape, self.patch_shape) if n_samples is None else n_samples
+
+        #TODO do i need the sample random index stuff???
+        '''self.sample_random_index = False if n_samples is None else True'''
 
     def __len__(self):
         return self._len
@@ -78,21 +86,13 @@ class HeatmapDataset(torch.utils.data.Dataset):
         return tuple(slice(start, start + psh) for start, psh in zip(bb_start, self.patch_shape))
 
     def _get_sample(self, index):
-        if self.sample_random_index:
-            index = np.random.randint(0, len(self.raw_images))
-        raw, label = self.raw_images[index], self.label_images[index]
-
-        # TODO this is specific for challenge zarr files now, maybe need to generalize in the future
-        # Yes ;). We can discuss this soon.
-
-        # zarr_file = zarr.open(f"{raw}/VoxelSpacing10.000/denoised.zarr/0", mode='r')
-        zarr_file = zarr.open(os.path.join(raw, "VoxelSpacing10.000", "denoised.zarr", "0"), mode='r')
-
-        raw = zarr_file
+        #TODO do i need the sample random index stuff???
+        '''if self.sample_random_index:
+            index = np.random.randint(0, len(self.raw_images))'''
+        raw, label_path = self.raw_images[index], self.label_paths[index]
 
         have_raw_channels = raw.ndim == 4  # 3D with channels
         
-        shape = raw.shape
         prefix_box = tuple()
         if have_raw_channels:
             if shape[-1] < 16:
@@ -106,7 +106,7 @@ class HeatmapDataset(torch.utils.data.Dataset):
         raw_patch = np.array(raw[prefix_box + bb])
         # sigma is not really used in my create_heatmap ... TODO ?
         label_patch = create_heatmap(
-            label, raw.shape, eps=self.eps, sigma=self.sigma,
+            label_path, raw.shape, eps=self.eps, sigma=self.sigma,
             lower_bound=self.lower_bound, upper_bound=self.upper_bound, bb=bb
         )
 
@@ -122,7 +122,7 @@ class HeatmapDataset(torch.utils.data.Dataset):
                 raw_patch = np.array(raw[prefix_box + bb])
                 # sigma is not really used in my create_heatmap ... TODO ?
                 label_patch = create_heatmap(
-                    label, raw.shape, eps=self.eps, sigma=self.sigma,
+                    label_path, raw.shape, eps=self.eps, sigma=self.sigma,
                     lower_bound=self.lower_bound, upper_bound=self.upper_bound, bb=bb
                 )
                 sample_id += 1
@@ -141,7 +141,6 @@ class HeatmapDataset(torch.utils.data.Dataset):
         if self.raw_transform is not None:
             raw = self.raw_transform(raw)
 
-        #TODO label variable is maybe something else now, need to change? Also check for rest of file
         if self.label_transform is not None:
             labels = self.label_transform(labels)
 
